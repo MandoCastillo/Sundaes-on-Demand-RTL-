81e570b49b285c26abb15a1e78bc2b1329ccda5c
Armando Castillo <armandocaslop@gmail.com>
2022-09-10 14:45:28 -0500

    Test and create toppings

--- src/pages/entry/test/option.test.jsx
+++ src/pages/entry/test/option.test.jsx
@@ -6,8 +6,17 @@ test('Display image for each scoop option from server', async () => {
 
   const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
-  screen.debug();
 
   const altText = scoopImages.map(element => element.alt);
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+test('Display image for each topping option from server', async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingsImages = await screen.findAllByRole('img', { name: /topping$/i });
+  expect(toppingsImages).toHaveLength(3);
+
+  const altText = toppingsImages.map(element => element.alt);
+  expect(altText).toEqual(['Cherries topping', 'M&Ms topping', 'Hot fudge topping']);
 });
