23785ef6803e96cf7899ac32bf1e4d6c3706caa1
Armando Castillo <armandocaslop@gmail.com>
2022-09-26 22:28:03 -0500

    total updates test

--- src/pages/entry/order-entry.jsx
+++ src/pages/entry/order-entry.jsx
@@ -1,10 +1,15 @@
 import Options from './options';
+import { useOrderDetails } from '../../contexts/order-details';
 
 export default function OrderEntry() {
+  const [orderDetails] = useOrderDetails();
+
   return (
     <>
+      <h1>Design Your Sundae!</h1>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
+      <h2>Grand total: {orderDetails.totals.grandTotal}</h2>
     </>
   );
 }
--- src/pages/entry/test/total-updates.test.tsx
+++ src/pages/entry/test/total-updates.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '../../../test-utils/testing-library.utils';
 
 import userEvent from '@testing-library/user-event';
 import Options from '../options';
-import { OrderDetailsProvider } from '../../../contexts/order-details';
+import OrderEntry from '../../entry/order-entry';
 
 test('update scoop subtotal when scoops change', async () => {
   render(<Options optionType="scoops" />);
@@ -24,4 +24,96 @@ test('update scoop subtotal when scoops change', async () => {
   await userEvent.type(chocolateInput, '2');
 
   expect(scoopsSubtotal).toHaveTextContent('6.00');
+});
+
+test('update toppings subtotal when topping change', async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingsSubtotal = screen.getByText('Toppings total: $', { exact: false });
+  expect(toppingsSubtotal).toHaveTextContent('0.00');
+
+  const cherriesCheckbox = await screen.findByRole('checkbox', {
+    name: 'Cherries',
+  });
+  await userEvent.click(cherriesCheckbox);
+  expect(cherriesCheckbox).toBeChecked();
+  expect(toppingsSubtotal).toHaveTextContent('1.50');
+
+  const hotFudgeCheckbox = screen.getByRole('checkbox', {
+    name: 'Hot fudge',
+  });
+  await userEvent.click(hotFudgeCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent('3.00');
+
+  await userEvent.click(cherriesCheckbox);
+  await userEvent.click(hotFudgeCheckbox);
+  expect(toppingsSubtotal).toHaveTextContent('0.00');
+});
+
+describe('grand total', () => {
+  test('grand total updates properly if scoop is added first', async () => {
+    render(<OrderEntry />);
+
+    const grandTotal = screen.getByRole('heading', { name: /grand total: \$/i });
+
+    expect(grandTotal).toHaveTextContent('0.00');
+
+    const vanillaInput = await screen.findByRole('spinbutton', {
+      name: 'Vanilla',
+    });
+    await userEvent.clear(vanillaInput);
+    await userEvent.type(vanillaInput, '1');
+    expect(grandTotal).toHaveTextContent('2.00');
+
+    const cherriesCheckbox = await screen.findByRole('checkbox', {
+      name: 'Cherries',
+    });
+    await userEvent.click(cherriesCheckbox);
+    expect(grandTotal).toHaveTextContent('3.50');
+  });
+
+  test('grand total updates properly if topping is added first', async () => {
+    render(<OrderEntry />);
+
+    const grandTotal = screen.getByRole('heading', { name: /grand total: \$/i });
+
+    const cherriesCheckbox = await screen.findByRole('checkbox', {
+      name: 'Cherries',
+    });
+    await userEvent.click(cherriesCheckbox);
+    expect(grandTotal).toHaveTextContent('1.50');
+
+    const vanillaInput = await screen.findByRole('spinbutton', {
+      name: 'Vanilla',
+    });
+    await userEvent.clear(vanillaInput);
+    await userEvent.type(vanillaInput, '2');
+    expect(grandTotal).toHaveTextContent('5.50');
+  });
+
+  test('grand total updates properly if item is removed', async () => {
+    render(<OrderEntry />);
+
+    const grandTotal = screen.getByRole('heading', { name: /grand total: \$/i });
+
+    const cherriesCheckbox = await screen.findByRole('checkbox', {
+      name: 'Cherries',
+    });
+    await userEvent.click(cherriesCheckbox);
+    expect(grandTotal).toHaveTextContent('1.50');
+
+    const vanillaInput = await screen.findByRole('spinbutton', {
+      name: 'Vanilla',
+    });
+    await userEvent.clear(vanillaInput);
+    await userEvent.type(vanillaInput, '3');
+    expect(grandTotal).toHaveTextContent('7.50');
+
+    await userEvent.click(cherriesCheckbox);
+    expect(grandTotal).toHaveTextContent('6.00');
+
+    await userEvent.clear(vanillaInput);
+    await userEvent.type(vanillaInput, '1');
+    expect(grandTotal).toHaveTextContent('2.00');
+  });
 });
